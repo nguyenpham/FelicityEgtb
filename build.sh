24200618ce469367d62195ae52649ee3e0077e3d
Nguyen Pham <phhnguyen@gmail.com>
2024-05-09 09:18:40 +1000

    update scripts

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ gcc -std=c99 -c ../src/lzma/*.c -D_7ZIP_ST
 g++ -c ../src/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_CHESS_
 g++ -c ../src/base/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_CHESS_
 g++ -c ../src/chess/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_CHESS_
-g++ -c ../src/egtb/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_CHESS_
-g++ -c ../src/egtbgen/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_CHESS_
+g++ -c ../src/fegtb/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_CHESS_
+g++ -c ../src/fegtbgen/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_CHESS_
 
 g++ -o fegtb *.o
 rm *.o
--- buildxq.sh
+++ buildxq.sh
@@ -7,8 +7,8 @@ gcc -std=c99 -c ../src/lzma/*.c -D_7ZIP_ST
 g++ -c ../src/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_XQ_
 g++ -c ../src/base/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_XQ_
 g++ -c ../src/xq/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_XQ_
-g++ -c ../src/egtb/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_XQ_
-g++ -c ../src/egtbgen/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_XQ_
+g++ -c ../src/fegtb/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_XQ_
+g++ -c ../src/fegtbgen/*.cpp -std=c++17 -O2 -DNDEBUG -D_FELICITY_XQ_
 
 g++ -o fegtb *.o
 rm *.o
